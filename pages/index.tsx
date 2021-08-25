f64b74a0af04178519732fd8b05e1278fe3e1a2b
Ben Ma <git.app@benjaminma.com>
2021-08-25 00:12:44 -0700

    Add hacky race countdown

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,13 +4,28 @@ import React from "react";
 // import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
-const pad = (n: string, d: number) => {
-  return n.padStart(d, "0");
-};
+function getRaceMessage(timerState: number, raceTime: number): string {
+  if (timerState === 0) {
+    return "Ready to race?";
+  }
+
+  const currTime = new Date().getTime();
+  if (currTime > raceTime) {
+    return "GO!!!";
+  }
+  const diffTime = currTime - raceTime;
+  if (diffTime <= 1000) return "3";
+  else if (diffTime <= 2000) {
+    return "2";
+  }
+  return "1";
+}
 
 const Home: NextPage = () => {
   const [timerState, setTimerState] = React.useState(0);
   const [startTime, setStartTime] = React.useState(new Date().getTime());
+  const [raceTime, setRaceTime] = React.useState(new Date().getTime());
+  const [raceMessage, setRaceMessage] = React.useState("Ready to race?");
   const [displayUnlit, setDisplayUnlit] = React.useState("0:00.000");
   const [displayLit, setDisplayLit] = React.useState("");
   const handleStart = React.useCallback(
@@ -18,8 +33,10 @@ const Home: NextPage = () => {
       if (timerState === 0) {
         setTimerState(1);
         setStartTime(new Date().getTime());
+        setRaceTime(new Date().getTime() + 3000);
       } else {
         setTimerState(0);
+        setRaceMessage("Race again?");
       }
     },
     [timerState]
@@ -29,11 +46,28 @@ const Home: NextPage = () => {
 
     if (timerState === 1) {
       intervalId = setInterval(() => {
-        const currTime = new Date();
-        const diffTime = currTime.getTime() - startTime;
+        const currTime = new Date().getTime();
+        let diffTime = raceTime - currTime;
+
+        if (raceTime > currTime) {
+          if (diffTime <= 1000) {
+            setRaceMessage("3... 2... 1...");
+          } else if (diffTime <= 2000) {
+            setRaceMessage("3... 2...");
+          } else if (diffTime <= 3000) {
+            setRaceMessage("3...");
+          }
+          setDisplayUnlit("0:00.000");
+          setDisplayLit("");
+          return;
+        }
+
+        setRaceMessage("GO!!!");
+
+        diffTime = currTime - raceTime;
         const diffDate = new Date(diffTime);
         const m = diffDate.getMinutes().valueOf();
-        const ss = diffDate.getSeconds().valueOf(); // toString().padStart(2, "0");
+        const ss = diffDate.getSeconds().valueOf();
         const sss = diffDate.getMilliseconds().toString().padStart(3, "0");
         let unlit = "";
         if (m === 0 && ss < 10) {
@@ -44,7 +78,7 @@ const Home: NextPage = () => {
         setDisplayUnlit(unlit);
         let lit;
         if (m > 0) {
-          lit = `${m}:${ss}.${sss}`;
+          lit = `${m}:${ss.toString().padStart(2, "0")}.${sss}`;
         } else {
           lit = `${ss}.${sss}`;
         }
@@ -52,8 +86,10 @@ const Home: NextPage = () => {
       }, 16);
     }
 
-    return () => clearInterval(intervalId);
-  }, [timerState, startTime]);
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, [timerState, startTime, raceTime]);
 
   return (
     <div className={styles.container}>
@@ -70,6 +106,7 @@ const Home: NextPage = () => {
           <a href="#" className={styles.navlogo}>
             creature-chrono
           </a>
+          <span></span>
           <a href="#">Race</a>
           {/* <a href="#">Hi-Score</a> */}
           <a href="#">Settings</a>
@@ -82,9 +119,7 @@ const Home: NextPage = () => {
           <span className={styles.racetimer_lit}>{displayLit}</span>
         </div>
 
-        <h1 className={styles.title}>
-          {timerState === 0 ? "Ready to race?" : "GO!!!"}
-        </h1>
+        <h1 className={styles.title}>{raceMessage}</h1>
       </main>
 
       <footer className={styles.footer}>
