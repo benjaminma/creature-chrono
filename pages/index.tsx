504fd91ec717b63f081429cb20381f0fee93fc53
Ben Ma <git.app@benjaminma.com>
2021-08-24 22:12:50 -0700

    Add mock start button, block out start screen, clean-up CSS

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import Image from "next/image";
+// import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
@@ -35,16 +35,9 @@ const Home: NextPage = () => {
       </main>
 
       <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{" "}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
+        <div className={styles.btnshadow}>
+          <button className={styles.btnstart}>Start</button>
+        </div>
       </footer>
     </div>
   );
